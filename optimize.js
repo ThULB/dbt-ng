52b8904be022216f0338a3a9d1aa76029a38ab9b
René Adler <rene.adler@uni-jena.de>
2018-08-06 12:06:13 +0200

    fix some errors

--- optimize.js
+++ optimize.js
@@ -11,28 +11,35 @@ const distDir = process.argv[2];
 const walk = (dir, done) => {
 	let results = [];
 	fs.readdir(dir, (err, list) => {
-		if (err) return done(err);
-	    
+		if (err) return { 
+			done(err) 
+		};
+
 		let pending = list.length;
-	    if (!pending) return done(null, results);
-	    
-	    list.forEach((file) => {
-	    	file = path.resolve(dir, file);
-	    	fs.stat(file, (err, stat) => {
-	    		if (stat && stat.isDirectory()) {
-	    			walk(file, (err, res) => {
-	    				results = results.concat(res);
-	    				if (!--pending) done(null, results);
-	    			});
-	    		} else {
-	    			results.push(file);
-	    			if (!--pending) done(null, results);
-	    		}
-	    	});
-	    });
+		if (!pending) {
+			return done(null, results);
+		}
+
+		list.forEach((file) => {
+			file = path.resolve(dir, file);
+			fs.stat(file, (err, stat) => {
+				if (stat && stat.isDirectory()) {
+					walk(file, (err, res) => {
+						results = results.concat(res);
+						if (!--pending) {
+							done(null, results);
+						}
+					});
+				} else {
+					results.push(file);
+					if (!--pending) {
+						done(null, results);
+					}
+				}
+			});
+		});
 	});
 };
-
 
 if (!distDir) {
 	console.error("Need \"dist\" path!");
